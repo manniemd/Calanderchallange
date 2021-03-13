d84919f60c7f38e52cb84b66402a97c38d56f3b6
manuel duron <manniemd@gmail.com>
2021-03-13 13:42:23 -0500

    added to java save feature and color change.

--- Develop/java.js
+++ Develop/java.js
@@ -12,3 +12,47 @@ var getDate = function () {
 };
 setInterval(getDate, 1000);
 
+var changeTextarea = function () {
+
+    var currentHour = moment().format('HH');
+    var hourNum = +currentHour;
+    var timeId = "#" + currentHour;
+
+    for (var i = 6; i < 18; i++) {
+        if (i == hourNum) {
+            $(timeId).find("textarea").addClass("present");
+        }
+        else if (i < hourNum) {
+            $("#" + i).find("textarea").addClass("past");
+        }
+        else {
+            $("#" + i).find("textarea").addClass("future");
+        }
+    }
+    lastUpdate = currentHour;
+};
+changeTextarea();
+
+$(".saveBtn").click(function () {
+
+    var eventTime = $(this)
+        .closest(".row")
+        .attr("id");
+
+    var eventText = $(this)
+        .closest(".row")
+        .find("textarea")
+        .val();
+
+    localStorage.setItem(eventTime, eventText);
+});
+var loadEvents = function () {
+
+    for (var i = 0; i < localStorage.length; i++) {
+        var time = localStorage.key(i);
+        var description = localStorage.getItem(time);
+        var timeId = ("#" + time);
+        $(timeId).find("textarea").val(description);
+    }
+};
+loadEvents();
